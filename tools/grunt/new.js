be241176d568b2e2e33d6b78550f174df1cedf7d
Jaime Caballero <me@jaicab.com>
2015-11-05 10:42:25 +0000

    Improve project creation

--- tools/grunt/new.js
+++ tools/grunt/new.js
@@ -7,27 +7,33 @@ module.exports = function(grunt, options) {
    	*   Remove the existing site directory, make it again, and run build all this.
    	*/
 
-   	grunt.registerTask('new', function(){
+   	grunt.registerTask('new', function(name){
 
-		var design_paths = [
-			'src',
-			options.paths.src.assets + '/' + grunt.config("design"),
-			options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.scss,
-			options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.img,
-			options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.js,
-			options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.fonts,
-			options.paths.src.flats + '/' + grunt.config("design"),
-			options.paths.src.themes + '/' + grunt.config("design"),
-			options.paths.build.flat.root + '/' + grunt.config("design"),
-			options.paths.build.dist.root + '/' + grunt.config("design")
-		];
+   		if(typeof name != 'string'){
+	   		grunt.fail.fatal("I need a project name: Give me one with grunt work:dev:design_name");
+	   	}else{
+	   		grunt.config.set('design', name);
 
-		// Create folders
-		design_paths.forEach(function(path){
-			if(!grunt.file.isDir(path)) grunt.file.mkdir(path);
-		});
+			var design_paths = [
+				'src',
+				options.paths.src.assets + '/' + grunt.config("design"),
+				options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.scss,
+				options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.img,
+				options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.js,
+				options.paths.src.assets + '/' + grunt.config("design") + '/' + options.paths.assets.fonts,
+				options.paths.src.flats + '/' + grunt.config("design"),
+				options.paths.src.themes + '/' + grunt.config("design"),
+				options.paths.build.flat.root + '/' + grunt.config("design"),
+				options.paths.build.dist.root + '/' + grunt.config("design")
+			];
 
-		grunt.task.run('sync:sass_init', 'sync:js_init', 'sync:flat_init');
+			// Create folders
+			design_paths.forEach(function(path){
+				if(!grunt.file.isDir(path)) grunt.file.mkdir(path);
+			});
+
+			grunt.task.run('sync:sass_init', 'sync:js_init', 'sync:flat_init');
+		}
 
    	});
 
--- tools/grunt/work.js
+++ tools/grunt/work.js
@@ -1,29 +1,29 @@
 module.exports = function(grunt, options) {
 
-	   /**
-   	* Set project to work on.
-   	*
-   	* grunt work:dev:craftedpeople
-   	*   Remove the existing site directory, make it again, and run build all this.
-   	*/
+      /**
+      * Set project to work on.
+      *
+      * grunt work:dev:craftedpeople
+      *   Remove the existing site directory, make it again, and run build all this.
+      */
 
-   	grunt.registerTask('work', function(mode, name){
+      grunt.registerTask('work', function(mode, name){
 
          // Check available methods
          if(mode !== "dev" && mode !== "deploy"){
             grunt.fail.fatal("Sorry mate, '" + mode + "' mode is not an option.");
          }
 
-   		// If no option, no nothing
-	   	if(typeof name != 'string'){
-	   		grunt.fail.fatal("I need a project name: Give me one with grunt work:dev:design_name");
-	   	}else{
-	   		grunt.config.set('design', name);
-            grunt.task.run('clean:reset', 'new');
+         // If no option, no nothing
+         if(typeof name != 'string'){
+            grunt.fail.fatal("I need a project name: Give me one with grunt work:dev:design_name");
+         }else{
+            grunt.config.set('design', name);
+            grunt.task.run('clean:reset', 'new:<%= grunt.config("design") %>');
 
             if(mode === "dev") grunt.task.run('dev');
             if(mode === "deploy") grunt.task.run('build'); 
-   		}
-   	});
+         }
+      });
 
 }
